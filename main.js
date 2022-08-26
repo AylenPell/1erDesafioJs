9188888df8a7e720d5ff0f5a87a5092d765d5a19
Aylen Pell <aylen.pell@gmail.com>
2022-08-26 19:54:08 -0300

    solucionada la carga de html first

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
-let randomNum = Math.round(Math.random()*10);
-let numElegido = parseInt(prompt("Adiviná el número que estoy pensando, tenés 3 intentos! Una pista? Está entre 0 y 10..."));
-let i = 0;
-
-while (numElegido !== randomNum && i<2){
-    i ++;
-    numElegido = parseInt(prompt("No es es el número correcto! Volvé a intentarlo"));
+function init(){
     
+
+let randomNum = Math.round(Math.random()*10);
+    let numElegido = parseInt(prompt("Adiviná el número que estoy pensando, tenés 3 intentos! Una pista? Está entre 0 y 10..."));
+    let i = 0;
+
+    while (numElegido !== randomNum && i<2){
+        i ++;
+        numElegido = parseInt(prompt("No es es el número correcto! Volvé a intentarlo"));
+
+    }
+
+    if (numElegido === randomNum) {
+        alert ("Muy bien! Adivinaste!");
+    }else if (numElegido !== randomNum){
+    alert (`Se agotaron tus 3 intentos! :( El número era el ${randomNum}... Actualizá la página para volver a intentarlo.`);
+    }
+    alert ("Actualizá la página para volver a intentarlo.");
 }
 
-if (numElegido === randomNum) {
-    alert ("Muy bien! Adivinaste!");
-}else if (numElegido !== randomNum){
-alert (`Se agotaron tus 3 intentos! :( El número era el ${randomNum}... Actualizá la página para volver a intentarlo.`);
-}
-alert ("Actualizá la página para volver a intentarlo.");
+    window.onload = init;
+    //init();
